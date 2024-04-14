3d35be97bbfc93925ecd193f2b6afa7fab903086
laiba-javaid <laibajavaid423@gmail.com>
2024-04-14 18:42:31 +0500

    new commit

--- assets/js/App.js
+++ assets/js/App.js
@@ -6,25 +6,50 @@ $(document).ready(function(){
         $("#welcome-message").toggle("slow");
     });
 
-    //Grid Animation when clicking on button
+    //Animation when clicking on button
     $("#teamButton").click(function(){
-        $(".card").animate({
-            height: '600px'
-        }, 5000, function() {
-            // Animation complete.
-        });
+        $('html, body').animate({
+            scrollTop: $(".team-section").offset().top
+          }, 800);
     });
 
-    $(".card").hover(
+    //Grid animation when hovering over grid items or cards
+    $(".card" ).hover(function(){
+        $(this).animate({
+            height: '+=150px',
+            marginTop: '-=15px',       
+            },300);     
+        $(this).css({ transform: "scale(1.08)",cursor:"pointer"});
+        $(this).find("h5").css({"color":"#9F522B", "font-weight":"bold", "font-size":"1.5em"});
+    },
         function(){
-            $(this).animate({ height: '900px' }, "slow");
-            $(this).animate({fontSize: '3em'}, "slow");
-            //$(this).find("h5").css({ "color": "#9F522B", "font-size": "1.5em" });
-        },
-        function(){
-            $(this).animate({ height: '' }, 200); // Reset height to default
-            $(this).find("h5").css({ "color": "", "font-size": "" }); // Reset font color and size
-        }
+        $(this).animate({
+                height: '-=150px',
+                marginTop: '+=15px',              
+            },300);
+            $(this).css( {transform: "",cursor:""});
+        $(this).find("h5").css({"color":"", "font-weight":"", "font-size":""});
+    }
     );
+
+    //Modal Image Gallery
+    $('.c-img').click(function(){
+        // Get the source of the clicked image
+        var src = $(this).attr('src');
+        
+        // Set the source of the modal image
+        $('#modalImage').attr('src', src);
+
+        // Show the modal
+        $('#imageModal').modal('show');
+    });
+
+
+    //Adjusting padding top of carousel items
+
+
+
+
+
 });
 
---
+++ scripts.js
@@ -0,0 +1,18 @@
+
+
+
+" Hello World ".trim(); // "Hello World"
+"Hello World".length(); // 11
+"Hello World".charAt(1); // "e"
+"Hello World".indexOf("o"); // 4
+"Hello World".replace("World", "Universe"); // "Hello Universe"
+"Hello World".startsWith("Hello"); // true
+"Hello World".endsWith("d"); // true
+"Hello World".includes("World"); // true
+"Hello World".match(/[A-Z]/g); // ["H", "W"]
+"Hello World".split(" "); // ["Hello", "World"]
+"Hello World".repeat(2); // "Hello WorldHello World"
+"Hello World".slice(1, 5); // "ello"
+"Hello World".substring(6,10); // "Worl"
+"Hello World".toLowerCase(); // "hello world"
+"Hello World".toUpperCase(); // "HELLO WORLD"
